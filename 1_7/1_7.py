254e70d846b7cedf0b3580a2a3c4e2ede074dcae
Braden <braden.coates@hotmail.com>
2019-02-13 08:16:48 -0500

    Update 1_7.py

--- 1_7/1_7.py
+++ 1_7/1_7.py
@@ -4,31 +4,43 @@
 #                Program: 1_7                  #
 #----------------------------------------------#
 
-import statistics as st
+from random import * #imports all of random
 
+#setting up dictionary and boolean
 studDict = dict()
 eMarks = True
 
-print("\nWelcome to my program\nHelp for list of commands\n")
+print("\nWelcome to my program")
 
-students = int(input("How many students in the class: "))
+students = int(input("\nHow many students in the class: ")) #asks for number of students
 
+#goes through each student asking for marks
 while students > 0:
     tempMark = list()
-    studName = input("Students name: ")
+    studName = input("\nStudents name: ")
 
     while eMarks:
-        nMark = input("\ntype 'next' for next student or enter a mark ")
+        nMark = input("\nEnter a mark or type 'next' for next student or 'random' for a random number of random marks ")
 
         if nMark == "next":
             break
+        elif nMark.isnumeric():
+            tempMark.append(int(nMark))
+        elif nMark == "random":
+            numOMarks = randint(1, 8)
+            while numOMarks > 0:
+                numOMarks -= 1
+                tempMark.append(randint(0, 100))
+                break
         else:
-            tempMark.append(nMark)
+            print("\nNot a number or a valid command")
 
     students -= 1
     studDict.update( {(studName) : (tempMark)} )
-print(studDict)
 
+#displays each students average than overall average
 for key in studDict.keys():
+    print ("\n{}'s average is {}".format(key, sum(studDict[key])/len(studDict[key]) ))
     nums = list(studDict[key])
-    print (st.mean(nums)) 
+
+print ("\nClass average is {}".format(sum(nums)/len(nums))) 
