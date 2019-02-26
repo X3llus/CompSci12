5f59734b8adfc9a5f9f598f1cf77842edd553a5d
Braden <braden.coates@hotmail.com>
2019-02-26 15:27:53 -0500

    updates

--- 1_15/dateConv.py
+++ 1_15/dateConv.py
@@ -9,18 +9,18 @@ def check(inp): #check inputs to make sure the date is correct
         m = int(inp[0]+inp[1])               #month
         d = int(inp[2]+inp[3])               #day
         y = int(inp[4]+inp[5]+inp[6]+inp[7]) #year
-        if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12: #checks for months with 31 days
-            if d <= 31:
-                return True
-        elif m == 4 or m == 6 or m == 9 or m == 11: #checks for months with 30 days
-            if d <= 30:
-                return True
+        if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12) and d <= 31 and d > 0: #checks for months with 31 days
+            # if d <= 31 and d > 0:
+            return True
+        elif (m == 4 or m == 6 or m == 9 or m == 11) and d <= 30 and d > 0: #checks for months with 30 days
+            # if d <= 30 and d > 0:
+            return True
         elif m == 2:
             if y % 4 == 0 and (not y % 100 == 0 or y % 400 == 0): #checks if it is a leap year
-                if d <= 29:
+                if d <= 29 and d > 0:
                     return True
             else:
-                if d <= 28:
+                if d <= 28 and d > 0:
                     return True
         else:
             return False
@@ -50,6 +50,6 @@ def main(): #main function
     date.append(tempStr[0]+tempStr[1])                       #month
     date.append(tempStr[4]+tempStr[5]+tempStr[6]+tempStr[7]) #year
 
-    print ("{} {} {}".format(int(date[0]), month[date[1]], date[2]))
+    print ("{} {} {}".format(int(date[0]), month[date[1]], int(date[2])))
 
 main()
